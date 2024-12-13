09f121312d6aac9909268e5f8629782d26c01283
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-12-13 12:14:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <input type="text" />
+      <button type="button">cadastrar</button>
+      <ul>
+        <li>estudar typescript</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
